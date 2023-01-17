c9bae81ad86bf8cfefd4bf39d381dc50da242bca
Sayan Chanda <mysayanchanda@gmail.com>
2023-01-17 19:55:32 +0530

    Unity Collab Merge

---
+++ Assets/temp/startup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using  UnityEngine.SceneManagement;
+
+public class startup : MonoBehaviour
+{
+
+    public void gdhs()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
